7fa5465b700841a878e4b72080a6c3e952acae05
Rai <rai@rbutera.com>
2017-11-03 18:07:07 +0000

    made basic version of template

--- extension/src/index.js
+++ extension/src/index.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
+import Oxo from "./oxo";
+import "./oxo.scss";
 
 console.log("hello world");
 
 var root = new Vue({
   el: "#oxo-root",
   data: {
-    message: "You loaded this page on " + new Date().toLocaleString()
+    message: "You loaded this page on " + new Date().toLocaleString(),
+    game: Oxo
   }
 });
